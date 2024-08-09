e71e96ab9ae90e14d194e311b2e71d100428595b
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-08-09 12:14:53 +0530

    cheking the value

--- 03_dataTypeConversion.js
+++ 03_dataTypeConversion.js
@@ -1,9 +1,17 @@
-let score = undefined;
+// let score = undefined;
 
-console.log( typeof score);
-console.log( typeof (score));
+// console.log( typeof score);
+// console.log( typeof (score));
 
-let valueInNumber = Number(score);
-console.log( typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log( typeof valueInNumber);
+// console.log(valueInNumber);
 
+// ********************Operation*********************************//
+
+// let value = 3
+
+// let negVlaue = -value
+// console.log(negVlaue);
+
+// console.log();
