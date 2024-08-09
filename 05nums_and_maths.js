3003374dd3f79d6b885c005cd7d6adcc75b4a511
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-08-09 14:01:26 +0530

    converting the number into precise value or fixed or we can round off fixed value

---
+++ 05nums_and_maths.js
@@ -0,0 +1,15 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumbers = 23.896
+
+console.log(otherNumbers.toPrecision(3));
+
+
+const hundreds = 100000
+console.log(hundreds.toLocaleString('en-IN'));
